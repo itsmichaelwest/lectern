829e760ebbc786846a2898e2d97a6562efdf7457
Michael West <maw86@aber.ac.uk>
2021-03-12 16:00:50 +0000

    depcheck and remove the file drop box

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -3,9 +3,6 @@ import axios from 'axios'
 import config from '../config'
 import { Helmet } from 'react-helmet'
 import { Link } from 'react-router-dom'
-import ContentLoader from 'react-content-loader'
-import { FileDrop } from 'react-file-drop'
-import { Formik } from 'formik'
 import Design from '../designSystem'
 import UploadVideo from '../functions/video/Upload'
 export default class Profile extends Component {
@@ -45,21 +42,6 @@ export default class Profile extends Component {
                     <div className="text-center mb-16">
                         <h1 className="text-6xl font-bold mt-16 mb-8">Upload Video</h1>
                         <p>By uploading a video, you agree to the <Link className="text-primary-600 hover:text-primary-900">Terms of Service</Link>.</p>
-                    </div>
-                    <div className="w-full md:w-3/5 h-64 mx-auto my-8 rounded-xl border-2 border-dashed border-primary-500 hover:border-primary-700 hover:bg-gray-100 transition-all">
-                        <FileDrop
-                            className="w-full h-full"
-                            targetClassName="flex items-center w-full h-full"
-                            onDrop={(files, event) => UploadVideo(files, event)}>
-                            <div className="text-md text-center my-auto mx-auto">
-                                <p>
-                                    Drag to upload
-                                </p>
-                                <p className="text-sm my-1">
-                                    Acceptable file types: MP4
-                                </p>
-                            </div>
-                        </FileDrop>
                     </div>
                 </div>
                 :
