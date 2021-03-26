a8206b4aef4141120be6546d0dc271aefca99bbb
Michael West <maw86@aber.ac.uk>
2021-03-26 13:53:34 +0000

    Some misc dev work for this week

--- src/components/atoms/video/VideoInformation.js
+++ src/components/atoms/video/VideoInformation.js
@@ -1,36 +1,41 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import config from '../../../config'
 import { Link } from 'react-router-dom'
 import Design from '../../../Design'
+import Dialog from '../../Dialog'
 
-export default class Video extends Component {
-    render () {
-        return (
-            <div>
-                <h1 className="text-xl font-bold">{this.props.title}</h1>
-                <p className="text-gray-600 mt-4">{this.props.description}</p>
-                <div className="flex justify-between items-center my-8">
-                    <div>
-                        <p>{this.props.views} views</p>
-                        <p>{this.props.date}</p>
-                    </div>
-                    <div>
-                        <button className={Design.Button} onClick={this.likeVideo}>
-                            {this.props.likes}
-                            Favorite
-                        </button>
-                    </div>
+export default function Video(props) {
+    return (
+        <div>
+            <h1 className="text-xl font-bold">{props.title}</h1>
+            <p className="text-gray-600 mt-4">{props.description}</p>
+            <div className="flex justify-between items-center my-8">
+                <div>
+                    <p>{props.views} views</p>
+                    <p>{props.date}</p>
                 </div>
-                <div className="flex justify-between items-center">
-                    <Link className="flex flex-row items-center font-bold" to={'/channel/' + this.props.id}>
-                        <div className="flex-initial rounded-full bg-primary h-8 w-8 mr-2">
-                            {/* put an image here ok */}
-                        </div>
-                        {this.props.channelName}
-                    </Link>
+                <div>
+                    {
+                        props.isCreator &&
+                        <button className={Design.ButtonDestructive + " mr-2"}>
+                            Delete
+                        </button>
+                    }
+                    <button className={Design.Button}>
+                        {props.faves}
+                        Favorite
+                    </button>
                 </div>
             </div>
-        )
-    }
+            <div className="flex justify-between items-center">
+                <Link className="flex flex-row items-center font-bold" to={'/channel/' + props.id}>
+                    <div className="flex-initial rounded-full bg-primary h-8 w-8 mr-2">
+                        {/* put an image here ok */}
+                    </div>
+                    {props.channelName}
+                </Link>
+            </div>
+        </div>
+    )
 }
--- src/components/skeletons/ProfileSkeleton.js
+++ src/components/skeletons/ProfileSkeleton.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import VideoRowSkeleton from './VideoRowSkeleton'
 
 export const ProfileSkeleton = () => (
     <>
     <div className="shimmer rounded max-w-md h-9 mx-auto"></div>
     <div className="shimmer rounded max-w-xs h-5 mx-auto my-4"></div>
     <div className="shimmer rounded w-32 h-9 mx-auto"></div>
-    <VideoRowSkeleton/>
     </>
 )
 
