59aae0c5a630a253b57d10029c52f2544b90d598
Michael West <maw86@aber.ac.uk>
2021-03-31 15:12:19 +0100

    Simplify AccountButton design

--- src/components/atoms/navigation/AccountButton.js
+++ src/components/atoms/navigation/AccountButton.js
@@ -10,8 +10,7 @@ export default class AccountButton extends React.Component {
         this.state = {
             displayName: null,
             isLogged: false,
-            menuSelected: false,
-            displayNone: true
+            menuSelected: false
         }
     }
 
@@ -49,35 +48,15 @@ export default class AccountButton extends React.Component {
     }
 
     toggleMenu = () => {
-        if (!this.state.menuSelected) {
-            this.setVisible(true)
-            setTimeout(() => this.setState({
-                menuSelected: true
-            }), 1);
-        } else {
-            this.setState({
-                menuSelected: false
-            })
-            setTimeout(() => this.setVisible(false), 650);
-        }
+        this.setState({
+            menuSelected: !this.state.menuSelected
+        })
     }
 
     hideMenu = () => {
         this.setState({
             menuSelected: false
         })
-    }
-
-    setVisible(state) {
-        if (state === true) {
-            this.setState({
-                displayNone: false
-            })
-        } else if (state === false) {
-            this.setState({
-                displayNone: true
-            })
-        }
     }
 
     render() {
@@ -90,39 +69,38 @@ export default class AccountButton extends React.Component {
                 </div>
             )
         } else {
-            let classes
-            let hiddenClasses
-
-            this.state.menuSelected
-            ?
-            classes = 'origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 transition-all transform'
-            :
-            classes = 'origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 transition-all transform scale-90 opacity-0'
-
-            this.state.displayNone
-            ?
-            hiddenClasses = ' hidden'
-            :
-            hiddenClasses = ' '
-
-            return (
-                <div className="relative inline-block text-left flex-initial" ref={node => this.node = node}>
-                    <div>
+            if (!this.state.menuSelected) {
+                return (
+                    <div className="relative inline-block text-left flex-initial" ref={node => this.node = node}>
                         <button type="button" className={Design.Button} id="options-menu" aria-haspopup="true" aria-expanded="true" ref={node => this.node = node} onClick={this.toggleMenu}>
                             {this.state.displayName}
-                            <svg className={`-mr-1 ml-2 h-5 w-5 transform transition-all ${this.state.menuSelected ? "rotate-180" : ""}`} viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+                            <svg className="-mr-1 ml-2 h-5 w-5" viewBox="0 0 20 20">
+                                <path d="M5.46967 7.46967C5.76256 7.17678 6.23744 7.17678 6.53033 7.46967L10 10.9393L13.4697 7.46967C13.7626 7.17678 14.2374 7.17678 14.5303 7.46967C14.8232 7.76256 14.8232 8.23744 14.5303 8.53033L10.5303 12.5303C10.2374 12.8232 9.76256 12.8232 9.46967 12.5303L5.46967 8.53033C5.17678 8.23744 5.17678 7.76256 5.46967 7.46967Z" fill="#6B7280"/>
                             </svg>
                         </button>
                     </div>
-                    <div className={classes + hiddenClasses} ref={node => this.node = node}>
-                        <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                            <Link to="/profile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem" onClick={this.hideMenu}>Your profile</Link>
-                            <a href="/auth/logout" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Sign out</a>
+                )
+            } else if (this.state.menuSelected) {
+                return (
+                    <>
+                    <div className="relative inline-block text-left flex-initial" ref={node => this.node = node}>
+                        <button type="button" className={Design.Button} id="options-menu" aria-haspopup="true" aria-expanded="true" ref={node => this.node = node} onClick={this.toggleMenu}>
+                            {this.state.displayName}
+                            <svg className="-mr-1 ml-2 h-5 w-5" viewBox="0 0 20 20">
+                                <path d="M5.46967 7.46967C5.76256 7.17678 6.23744 7.17678 6.53033 7.46967L10 10.9393L13.4697 7.46967C13.7626 7.17678 14.2374 7.17678 14.5303 7.46967C14.8232 7.76256 14.8232 8.23744 14.5303 8.53033L10.5303 12.5303C10.2374 12.8232 9.76256 12.8232 9.46967 12.5303L5.46967 8.53033C5.17678 8.23744 5.17678 7.76256 5.46967 7.46967Z" fill="#6B7280"/>
+                            </svg>
+                        </button>
+                        <div className="absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5" ref={node => this.node = node}>
+                            <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                                <Link to="/profile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem" onClick={this.hideMenu}>Your profile</Link>
+                                <a href="/auth/logout" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Sign out</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            )
+                    </>
+
+                )
+            }
         }
     }
 }
