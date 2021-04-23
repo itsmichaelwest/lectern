c6c3189c5abff485b58142554896f8bcc1143d38
Michael West <maw86@aber.ac.uk>
2021-04-23 15:21:46 +0100

    Prevent zoom in on Safari input focus

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -15,7 +15,7 @@ export default function Dialog(props) {
     
     return (
         <div className="fixed inset-0 w-screen h-screen bg-black bg-opacity-60 z-50 items-center flex p-4">
-            <div className="relative bg-white shadow-2xl max-w-max max-h-max p-8 rounded-xl mx-auto">
+            <div className="relative bg-white shadow-2xl max-w-max sm:max-w-xl max-h-max p-8 rounded-xl mx-auto">
                 <div>
                     {props.children}
                 </div>
